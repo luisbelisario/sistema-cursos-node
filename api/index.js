a1f88b74e00c91f4d9d1a88c4193a86b738241aa
luisbelisario <luisbls77@hotmail.com>
2023-06-16 16:11:12 -0300

    configuracoes iniciais node e sequelize

---
+++ api/index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const chalk = require('chalk')
+
+const app = express()
+
+app.use(bodyParser.json())
+
+const port = 3000
+
+app.get('/', (req, res) => res
+    .status(200)
+    .send({mensagem: 'Hello Nodemon'})
+)
+
+app.listen(port, () => console.log(chalk.green(`Server running on port ${port}`)))
+
+module.exports = app
