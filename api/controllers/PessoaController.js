5983bc4b157452b4dd8ea6b4cb614e05a83bf2bb
luisbelisario <luisbls77@hotmail.com>
2023-06-22 10:13:15 -0300

    criando controller de pessoas

---
+++ api/controllers/PessoaController.js
@@ -0,0 +1,14 @@
+const database = require('../models');
+// não preciso adicionar o index porque ele já busca pelo arquivo index.js nessa pasta
+
+class PessoaController {
+
+    static async findPessoas(req, res) {
+        try {
+            const pessoas = await database.Pessoas.findAll();
+            return res.status(200).json(pessoas);
+        } catch (error) {
+            res.status(500).json(error.message);
+        }
+    }
+}
--- api/index.js
+++ api/index.js
@@ -1,18 +1,18 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const chalk = require('chalk')
+const express = require('express');
+const bodyParser = require('body-parser');
+const chalk = require('chalk');
 
-const app = express()
+const app = express();
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
-const port = 3000
+const port = 3000;
 
 app.get('/', (req, res) => res
     .status(200)
     .send({mensagem: 'Hello Nodemon'})
-)
+);
 
-app.listen(port, () => console.log(chalk.green(`Server running on port ${port}`)))
+app.listen(port, () => console.log(chalk.green(`Server running on port ${port}`)));
 
-module.exports = app
+module.exports = app;
