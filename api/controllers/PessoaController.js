60f68359ed4fcf1234539478ef0ea75a6805aea0
luisbelisario <luisbls77@hotmail.com>
2023-06-29 11:14:07 -0300

    finalizado crud de pessoas

--- api/controllers/PessoaController.js
+++ api/controllers/PessoaController.js
@@ -11,6 +11,57 @@ class PessoaController {
             res.status(500).json(error.message);
         }
     }
+
+    static async findUmaPessoa(req, res) {
+        const { id } = req.params;
+        // tenho que o id colocar entre chaves para não dar erro
+        try {
+            const pessoa = await database.Pessoas.findOne( {
+                where: {
+                    id: Number(id)
+                }
+            } )
+            return res.status(200).json(pessoa);
+        } catch (error) {
+            res.status(500).json(error.message);
+        }
+    }
+
+    static async createPessoa(req, res) {
+        const novaPessoa = req.body;
+        try {
+            const pessoaCriada = await database.Pessoas.create(novaPessoa);
+            return res.status(200).json(pessoaCriada);
+        } catch (error) {
+            res.status(500).json(error.message);
+        }
+    }
+
+    static async updatePessoa(req, res) {
+        const { id } = req.params;
+        const novasInfos = req.body;
+        try {
+            await database.Pessoas.update(novasInfos, { where: {id: Number(id)} });
+            const pessoaAtualizada = await database.Pessoas.findOne( {
+                where: {
+                    id: Number(id)
+                }
+            } )
+            return res.status(200).json(pessoaAtualizada);
+        } catch (error) {
+            res.status(500).json(error.message);
+        }
+    }
+
+    static async deletePessoa(req, res) {
+        const { id } = req.params;
+        try {
+            await database.Pessoas.destroy({ where: {id: Number(id)} });
+            return res.status(200).json({mensagem : `Id ${id} deletado`});
+        } catch (error) {
+            res.status(500).json(error.message);
+        }
+    }
 }
 
 module.exports = PessoaController;
--- api/routes/pessoasRoute.js
+++ api/routes/pessoasRoute.js
@@ -4,5 +4,9 @@ const PessoaController = require('../controllers/PessoaController');
 const router = Router();
 
 router.get('/pessoas', PessoaController.findPessoas);
+router.get('/pessoas/:id', PessoaController.findUmaPessoa);
+router.post('/pessoas', PessoaController.createPessoa);
+router.put('/pessoas/:id', PessoaController.updatePessoa);
+router.delete('/pessoas/:id', PessoaController.deletePessoa);
 
 module.exports = router;
